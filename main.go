9c13da081df483544b4fd5adcaa741289ddf094d
Seth Back <sethjback@gmail.com>
2023-08-01 15:43:26 -0700

    interface

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"os"
 	"strconv"
@@ -12,32 +13,33 @@ func main() {
 		os.Exit(1)
 	}
 
+	fmt.Println(os.Args)
+
+	var op Operation
+
 	switch os.Args[1] {
 	case "add":
-		ai, err := convertArgs(os.Args[2:])
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-
-		a := NewAdder(ai[0], ai[1])
-
-		fmt.Println(a.Calculate())
+		op = &Adder{}
 	case "sub":
-		ai, err := convertArgs(os.Args[2:])
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-
-		s := NewSubber(ai[0], ai[1])
-
-		fmt.Println(s.Calculate())
+		op = &Subber{}
 	default:
 		fmt.Println("valid operations are add and sub")
 		os.Exit(1)
 	}
 
+	ai, err := convertArgs(os.Args[2:])
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	err = op.Args(ai)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	fmt.Println(op.Calculate())
 }
 
 func convertArgs(args []string) ([]int, error) {
@@ -55,6 +57,11 @@ func convertArgs(args []string) ([]int, error) {
 	return ai, nil
 }
 
+type Operation interface {
+	Args([]int) error
+	Calculate() int
+}
+
 // Adder is for basic addition
 type Adder struct {
 	num1 int // first number to add
@@ -65,20 +72,43 @@ func NewAdder(a, b int) *Adder {
 	return &Adder{num1: a, num2: b}
 }
 
+func (a *Adder) Args(numbers []int) error {
+	if len(numbers) > 2 {
+		return errors.New("we only support adding 2 numbers")
+	}
+	a.num1 = numbers[0]
+	a.num2 = numbers[1]
+
+	return nil
+}
+
 func (a *Adder) Calculate() int {
 	return a.num1 + a.num2
 }
 
 // Subber is for basic subtraction
 type Subber struct {
-	num1 int // first number to add
-	num2 int // second number to add
+	nums []int // numbers to subtract
 }
 
-func NewSubber(a, b int) *Subber {
-	return &Subber{num1: a, num2: b}
+func NewSubber(numbers []int) *Subber {
+	return &Subber{nums: numbers}
 }
 
-func (a *Subber) Calculate() int {
-	return a.num1 - a.num2
+func (s *Subber) Args(numbers []int) error {
+	s.nums = numbers
+	return nil
+}
+
+func (s *Subber) Calculate() int {
+	var ans int
+	for i, n := range s.nums {
+		if i == 1 {
+			ans = n
+			continue
+		}
+		ans -= n
+	}
+
+	return ans
 }
